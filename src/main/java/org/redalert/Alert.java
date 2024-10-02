3c77bdaf24cb2604e83ebcb4bededdfa6c4baba4
yoyobara <yotambarak6@gmail.com>
2024-10-02 16:28:26 +0300

    deserialized successfully a single objcet

--- src/main/java/org/redalert/Alert.java
+++ src/main/java/org/redalert/Alert.java
@@ -1,5 +1,31 @@
 package org.redalert;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonSetter;
+
 public class Alert {
+    @JsonProperty
     private String notificationId;
+
+    private LocalDateTime datetime;
+
+    @JsonProperty
+    private int threat;
+
+    @JsonProperty
+    private boolean isDrill;
+
+    @JsonProperty
+    public List<String> cities;
+
+    @JsonSetter("time")
+    public LocalDateTime setTheDateTime(int time) {
+        return Instant.ofEpochSecond(time).atZone(ZoneId.systemDefault()).toLocalDateTime();
+    }
 }
--- src/main/java/org/redalert/App.java
+++ src/main/java/org/redalert/App.java
@@ -1,10 +1,21 @@
 package org.redalert;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.ZoneOffset;
+import java.util.TimeZone;
+
+import com.fasterxml.jackson.databind.json.JsonMapper;
+
 /**
  * Hello world!
  */
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws IOException {
+        String jsonVal = Files.readString(Path.of("example.json"));
+        JsonMapper mapper = new JsonMapper();
+        Alert alert = mapper.readValue(jsonVal, Alert.class);
+        System.out.println(alert.cities);
     }
 }
